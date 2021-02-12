2b65f342907345f9d57ea95c4e0bb6105cb2e645
jglez <jglez.cs@gmail.com>
2021-02-12 16:31:15 -0600

    Wrote two tests and double checked for false positives - passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,51 @@
 import React from "react";
-import { render } from "@testing-library/react";
+// Get screen from react testing library
+import { render, screen } from "@testing-library/react";
+// Import userEvent
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  // Arrange
+  render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+  // Act
+  const heading = screen.getByRole('heading')
+
+  // Assert
+  expect(heading).toBeInTheDocument()
+  expect(heading).toHaveTextContent('Checkout Form')
+  // Double checked that it's not a false positive ✅
+});
+
+test("form shows success message on submit with form details", () => {
+  /// Arrange ///
+  render(<CheckoutForm />)
+
+  /// Act ///
+  const firstName = screen.getByLabelText(/first name/i)
+  const lastName = screen.getByLabelText(/last name/i)
+  const address = screen.getByLabelText(/address/i)
+  const city = screen.getByLabelText(/city/i)
+  const state = screen.getByLabelText(/state/i)
+  const zip = screen.getByLabelText(/zip/i)
+  const button = screen.getByRole('button', { name: /checkout/i })
+
+  // type into form
+  userEvent.type(firstName, 'Elliot')
+  userEvent.type(lastName, 'Alderson')
+  userEvent.type(address, '1216 Surf Avenue')
+  userEvent.type(city, 'New York City')
+  userEvent.type(state, 'New York')
+  userEvent.type(zip, '11224')
+
+  // click checkout button
+  userEvent.click(button)
+
+  const successMessage = screen.getByTestId('successMessage')
+
+  /// Assert ///
+  expect(successMessage).toBeInTheDocument() // double checked for false positive ✅
+});
